0986ba789cacfcfb47e8d7ff21791b774f12e470
Antonio32A <antonio.anzil@gmail.com>
2019-07-11 16:52:25 +0200

    Multi guild support

--- extensions/welcome/welcome.py
+++ extensions/welcome/welcome.py
@@ -5,23 +5,23 @@ import asyncio
 class Welcome(commands.Cog, name="Welcome"):
     def __init__(self, crab):
         self.crab = crab
-        self.verification = self.crab.config["verification"]
-        self.guild = self.crab.get_guild(self.crab.config["guild"])
-        self.info_channel = self.guild.get_channel(self.verification["info_channel"])
-        self.waiting_time = self.verification["waiting_time"]
-        self.verified_role = self.guild.get_role(self.verification["verified_role"])
-        self.channel_name = self.verification["channel_name"]
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
+        guild = member.guild
+        verification = self.crab.config["guilds"][str(guild.id)]["verification"]
+        info_channel = guild.get_channel(verification["info_channel"])
+        waiting_time = verification["waiting_time"]
+        verified_role = guild.get_role(verification["verified_role"])
+        channel_name = verification["channel_name"]
         overwrites = {
-            self.guild.default_role: discord.PermissionOverwrite(read_messages=False),
+            guild.default_role: discord.PermissionOverwrite(read_messages=False),
             member: discord.PermissionOverwrite(read_messages=True, send_messages=False),
         }
-        channel = await self.guild.create_text_channel(self.channel_name + str(member.id), overwrites=overwrites)
+        channel = await guild.create_text_channel(channel_name + str(member.id), overwrites=overwrites)
 
-        async for message in self.info_channel.history(limit=None):
-            embed = discord.Embed(color=message.author.color)
+        async for message in info_channel.history(limit=None):
+            embed = discord.Embed(color=discord.Color(0x191919))
             embed.description = message.content
             try:
                 embed.set_image(url=message.attachments[0].url)
@@ -29,20 +29,20 @@ class Welcome(commands.Cog, name="Welcome"):
                 pass
             await channel.send(embed=embed)
 
-        time_left = self.waiting_time-10
+        time_left = waiting_time-5
         embed = discord.Embed(color=discord.Color(0x191919))
         embed.set_author(name="Welcome!", icon_url=member.avatar_url)
-        embed.description = f"Please read the info above and wait {self.waiting_time} seconds!"
+        embed.description = f"Please read the info above and wait {waiting_time} seconds!"
         embed.add_field(name="Time left", value=f"{time_left} seconds.")
         message = await channel.send(embed=embed)
 
         while time_left >= 0:
-            await asyncio.sleep(10)
+            await asyncio.sleep(5)
             time_left -= 5
-            embed.set_field_at(index=0, name="Time left", value=f"{time_left+10} seconds.")
+            embed.set_field_at(index=0, name="Time left", value=f"{time_left+5} seconds.")
             await message.edit(embed=embed)
         try:
-            await member.add_roles(self.verified_role)
+            await member.add_roles(verified_role)
         except:
             pass
         return await channel.delete()
